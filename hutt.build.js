42940bbcafdf72b4ebbee080f13d90025cca9806
Venkat Peri <venkatperi@gmail.com>
2017-08-25 00:02:41 -0400

    updates

--- hutt.build.js
+++ hutt.build.js
@@ -1,9 +1,24 @@
 apply( { plugins: ['babel', 'sass'] } )
-includeFlat( 'hutt-cli', 'hutt-plugin-sourceset-js' )
+
+includeFlat(
+  'hutt-cli',
+  'hutt-plugin-sourceset-js',
+  'hutt-plugin-sourceset-sass',
+  'hutt-plugin-babel',
+  'hutt-plugin-sass',
+)
 
 babel( {
   presets: ['es2015'],
   sourceMaps: true,
+} )
+
+sourceSets( () => {
+  main( () => {
+    js( () => {
+      include( 'index.js' )
+    } )
+  } )
 } )
 
 task( 'default', { dependsOn: ['build'] } )
